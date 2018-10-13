fe42c2f1892003861eca1755e2d2e4483e50681b
Aanuragkumarsrivastava <42467453+Aanuragkumarsrivastava@users.noreply.github.com>
2018-10-13 19:29:17 +0530

    create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#calculating the area of square by taking the sideof the square
+n = int(input("enter the length of the rectangle:\n"))
+#taking the length of rectangle from the user
+m = int(input("enter the breadth of rectangle:\n"))
+#taking the breadth of rectangle from the user
+a = int(n) * int(m) 
+#finding the area of rectangle by multiplying length and breadth
+print("the area of square is:\n" + str(a)) 
+#printing the area which we have calculated
